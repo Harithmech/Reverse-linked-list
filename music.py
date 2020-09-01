6df625277c5de87ea8b077964b63924dc5ac38d9
harith m <harithmech@gmail.com>
2020-09-01 22:41:29 +0530

    elements added to linked list

--- music.py
+++ music.py
@@ -1,4 +1,6 @@
 #creating a linked list
+#adding elements to linke list
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -8,5 +10,20 @@ class Linkedlist:
     def __init__(self):
         self.head = None
 
+    def insertend(self, ele):
+        if self.head is None:
+            self.head = ele
+        else:
+            lastnode = self.head
+            while lastnode.next is not None:
+                lastnode = lastnode.next
+            lastnode.next = ele
+
+
 first_ele = Node(1)
+second_ele = Node(2)
+third_ele = Node(3)
 lis = Linkedlist()
+lis.insertend(first_ele)
+lis.insertend(second_ele)
+lis.insertend(third_ele)
