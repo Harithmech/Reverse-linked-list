b51f7fc0fa48a441b97cef1ab1c28ade4de9c404
harith m <harithmech@gmail.com>
2020-09-02 20:49:29 +0530

    added insert head node

--- Untitled.py
+++ Untitled.py
@@ -1,10 +1,13 @@
-#linked list
+#crete a linked list
+#append elements
+#print list
+#add element at head node
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-    def __repr__(self):
-        return self.data
+    """ def __repr__(self):
+        return self.data """
 
 class LinkedList:
     def __init__(self):
@@ -27,15 +30,24 @@ class LinkedList:
                 break
             print(currentnode.data)
             currentnode = currentnode.next
+
+    def addheadnode(self, ele):
+        temp = self.head
+        self.head = ele
+        ele.next = temp
+        del temp
+
 first_ele = Node("harith")
 second_ele = Node("harshith")
 third_ele = Node("shubha")
 fourth_ele = Node("Marappa")
+zeroth_ele = Node("-----------")
 
 link = LinkedList()
 link.append(first_ele)
 link.append(second_ele)
 link.append(third_ele)
 link.append(fourth_ele)
-
+link.addheadnode(zeroth_ele)
 link.printlist()
+
